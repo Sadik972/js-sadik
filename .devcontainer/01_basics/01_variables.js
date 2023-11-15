d3be712f10ff958628a29a33854bbdd965858dc8
Sadik972 <150897210+Sadik972@users.noreply.github.com>
2023-11-15 10:01:41 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144534
+let accountEmail = 'sadik007972@gmail.com'
+var accountPassword ="12345"
+accountCity = "Kadapa"
+let accountState;
+// accountId = 2322
+
+accountEmail ="sadik00000@gmail.com"
+
+console.table([accountId, accountEmail, accountPassword])
+
+console.log(accountState)
